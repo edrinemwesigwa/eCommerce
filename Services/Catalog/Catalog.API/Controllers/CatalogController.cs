db15070b4bf1ce2cd58e0dc0e4d31104a77cee70
edrineSubaru <edrineviper@gmail.com>
2025-08-18 22:32:50 +0400

    test again removed

--- Services/Catalog/Catalog.API/Controllers/CatalogController.cs
+++ Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -107,18 +107,6 @@ namespace Catalog.API.Controllers
             var command = new DeleteProductByIdCommand(id);
             var result = await _mediator.Send(command);
             return Ok(result);
-        }
-
-
-        [HttpDelete]
-        [Route("DeleteProduct")]
-        [ProducesResponseType(typeof(bool), (int)HttpStatusCode.OK)]
-        public async Task<ActionResult<ProductResponse>> DeleteProductTest(string id)
-        {
-            var command = new DeleteProductByIdCommand(id);
-            var result = await _mediator.Send(command);
-            return Ok(result);
-        }
-
+        }       
     }
 }
